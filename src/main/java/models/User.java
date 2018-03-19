bd9d308f02564f6951106e3f2747209b4673269a
TolunayToprakkaya <tolunaytoprakkaya@gmail.com>
2018-03-20 00:02:19 +0300

    19/03/2018 00:02

---
+++ src/main/java/models/User.java
@@ -0,0 +1,30 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class User {
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "userStatus")
+    private Integer userStatus;
+
+}
