49bf3ab1c1fdc27d4c550f383521b841ead97b5b
TolunayToprakkaya <tolunaytoprakkaya@gmail.com>
2018-03-23 12:56:14 +0300

    23/03/2018 12:56

--- src/main/java/repositories/UserRepository.java
+++ src/main/java/repositories/UserRepository.java
@@ -1,14 +1,9 @@
 package repositories;
 
 import models.User;
-import models.seedwork.AbstractEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 
 @RepositoryRestResource(path = "user", collectionResourceRel = "user")
 public interface UserRepository extends JpaRepository<User, Integer> {
--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -1,17 +1,6 @@
 package services;
 
-import models.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import repositories.UserRepository;
-
-import java.util.Optional;
-
 public interface UserService {
 
-    /*
-    Iterable<User> findAll();
-    Optional<User> findOne(Integer id);
-    User save(User user);
-    void delete(Integer id);
-    */
+
 }
