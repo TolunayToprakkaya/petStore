a363ee06da90333a04a155a2d39278507ed3d863
TolunayToprakkaya <tolunaytoprakkaya@gmail.com>
2018-04-08 17:23:49 +0300

    08/04/2018 17:23

--- src/main/java/repositories/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package repositories;
-
-import models.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "tag", collectionResourceRel = "tag")
-public interface TagRepository extends JpaRepository<Tag, Integer>{
-}
