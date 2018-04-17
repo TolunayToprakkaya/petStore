f5179809d59cc9e60750b909cf0cb83bbef4ca2b
Zafer Fatih Koyuncu <zaferfatih@gmail.com>
2018-04-17 06:43:52 +0200

    removed unnecessary code block

--- src/main/java/com/example/petStore/PetStoreApplication.java
+++ src/main/java/com/example/petStore/PetStoreApplication.java
@@ -2,11 +2,7 @@ package com.example.petStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableJpaRepositories("com.example.petstore.repositories")
-//@EntityScan("com.example.petstore.models")
 @SpringBootApplication
 public class PetStoreApplication {
 
